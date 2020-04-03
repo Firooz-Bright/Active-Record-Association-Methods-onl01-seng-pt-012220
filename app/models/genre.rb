90fd1b590640e19bddeb47fae325fe8b96de6fea
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-04-03 09:04:56 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,6 +15,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists.select{|x|  x.name}
+    self.artists.collect{|x|  x.name}
   end
 end
